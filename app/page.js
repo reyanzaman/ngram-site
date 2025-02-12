f4dba9a88bdc002d2ecb93400e1cc1c3b8c718f0
reyanzaman <reyanzaman.rz@gmail.com>
2025-02-12 12:32:14 +0600

    phase 1

--- app/page.js
+++ app/page.js
@@ -2,99 +2,70 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="items-center justify-items-center min-h-screen lg:px-8 md:px-6 px-6 lg:py-10 md:py-8 py-7 pb-20 gap-16">
+      <header className="w-full">
+        <h1 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="lg:text-4xl md:text-4xl text-3xl text-center font-bold">N-Gram Searching</h1>
+        <h3 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="lg:text-xl md:text-lg text-base text-center">on the Holy Quran</h3>
+        <hr className="w-full lg:my-6 my-3 border-zinc-400"></hr>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full lg:px-8 px-2 lg:py-10 py-6">
+        <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-6 gap-y-3 w-full text-center">
+          <div>
+            <h3 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="font-bold text-xl py-1 text-zinc-100">Bi-Gram</h3>
+            <div className="bg-[#1f2624] shadow-md rounded lg:p-6 p-3 lg:h-[40rem] h-[15rem] text-zinc-200 overflow-y-scroll overflow-x-auto lg:text-lg text-sm">
+              <ul className="">
+                <li className="py-1 text-nowrap text-green-200">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+                <li className="py-1 text-nowrap">Some Text1 Some Text1 Some Text1 Some Text1 Some Text1</li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h3 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="font-bold text-xl py-1 text-zinc-500">Tri-Gram</h3>
+            <div className="bg-[#1f2624] shadow-md rounded lg:p-6 p-3 lg:h-[40rem] h-[15rem] text-zinc-200">
+              <h1>Some Text3</h1>
+            </div>
+          </div>
+          <div>
+            <h3 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="font-bold text-xl py-1 text-zinc-500">Four-Gram</h3>
+            <div className="bg-[#1f2624] shadow-md rounded lg:p-6 p-3 lg:h-[40rem] h-[15rem] text-zinc-200">
+              <h1>Some Text4</h1>
+            </div>
+          </div>
+          <div>
+            <h3 style={{ fontFamily: "'Julius Sans One', sans-serif" }} className="font-bold text-xl py-1 text-zinc-500">Five-Gram</h3>
+            <div className="bg-[#1f2624] shadow-md rounded lg:p-6 p-3 lg:h-[40rem] h-[15rem] text-zinc-200">
+              <h1>Some Text5</h1>
+            </div>
+          </div>
         </div>
       </main>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
